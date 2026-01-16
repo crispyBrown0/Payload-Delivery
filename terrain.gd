1ca28308710ba4c3581b07c50ddcde0510b38a11
khlobbia <khlobbia@hotmail.com>
2026-01-16 14:45:04 -0800

    Collider syncs with PolygonShape in code
    that's it.

---
+++ terrain.gd
@@ -0,0 +1,12 @@
+extends StaticBody2D
+
+@onready var poly = $TerrainPoly
+@onready var colli = $TerrainColli as CollisionPolygon2D
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	colli.polygon = poly.polygon
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
