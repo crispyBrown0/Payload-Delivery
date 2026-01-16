43b9b82a00811f3109c058eb374aa88db8f3562d
khlobbia <khlobbia@hotmail.com>
2026-01-16 15:39:24 -0800

    Properly pushed
    -polygon and collider sync in code
    -random test object
    -parallax background
    -green outline on terrain

--- terrain.gd
+++ terrain.gd
@@ -2,9 +2,11 @@ extends StaticBody2D
 
 @onready var poly = $TerrainPoly
 @onready var colli = $TerrainColli as CollisionPolygon2D
+@onready var line = $TerrainLine
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	colli.polygon = poly.polygon
+	line.points = poly.polygon
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
