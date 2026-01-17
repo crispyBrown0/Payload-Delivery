9f43f9100b67ef91279d1a4c3e59c9daa955412a
khlobbia <khlobbia@hotmail.com>
2026-01-16 17:06:10 -0800

    Enemy and Explosion Basics
    -press 0 to kill enemy
    -enemy generates collider based on sprite automatically
    -spawned explosion is animated and SFX

---
+++ enemy.gd
@@ -0,0 +1,32 @@
+extends RigidBody2D
+
+@onready var sprite = $sprite
+@onready var collider = $collider
+var hp = 1
+@export var explosion: PackedScene
+
+func _ready() -> void:
+	var bitmap = BitMap.new()
+	bitmap.create_from_image_alpha(sprite.texture.get_image())
+	#print(str(bitmap.opaque_to_polygons(Rect2(Vector2.ZERO, sprite.texture.get_size()))))
+	collider.polygon = bitmap.opaque_to_polygons(Rect2(Vector2.ZERO, sprite.texture.get_size()))[0]
+	collider.position -= sprite.texture.get_size()/2
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_key_pressed(KEY_0):
+		damage(1)
+
+
+func damage(amt: int):
+	hp -= amt
+	if hp<=0:
+		die()
+
+func die():
+	var new_explosion = explosion.instantiate()
+	new_explosion.scale = sprite.texture.get_size()/100.0
+	add_child(new_explosion)
+	new_explosion.reparent(get_parent())
+	queue_free()
