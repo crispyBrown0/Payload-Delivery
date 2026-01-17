a36e155727d9cffbdb8e1611de4f27d82ccdb4c3
khlobbia <khlobbia@hotmail.com>
2026-01-16 20:42:02 -0800

    The Great Re-merging
    -it works

--- player_weapon.gd
+++ player_weapon.gd
@@ -14,3 +14,14 @@ func _process(delta: float) -> void:
 	var target_angle = (get_global_mouse_position() - global_position).angle()
 	rotation = rotate_toward(rotation, target_angle, rotation_speed * delta)
 	rotation_degrees = clamp(rotation_degrees, -90, 30)
+	
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("fire"):
+		var current_projectile = $Projectile
+		if current_projectile:
+			var new_projectile = load("res://projectile.tscn").instantiate()
+			get_tree().root.add_child(new_projectile)
+			new_projectile.global_transform = current_projectile.global_transform
+			new_projectile.freeze = false
+			new_projectile.linear_velocity = Vector2.RIGHT.rotated(global_rotation) * current_projectile.projectile_velocity
+			current_projectile.queue_free()
